57b76cc19f22fee75fbfdd0e6a2d6ac5b268f415
arturgawlik <artur.gawlik@icloud.com>
2025-06-14 14:09:31 +0200

    cleanup after prev attempts

--- configure.py
+++ configure.py
@@ -61,8 +61,7 @@ maglev_enabled_architectures = ('x64', 'arm', 'arm64')
 shareable_builtins = {'cjs_module_lexer/lexer': 'deps/cjs-module-lexer/lexer.js',
                      'cjs_module_lexer/dist/lexer': 'deps/cjs-module-lexer/dist/lexer.js',
                      'undici/undici': 'deps/undici/undici.js',
-                     'amaro/dist/index': 'deps/amaro/dist/index.js',
-                     'es_module_lexer/lexer': 'deps/es-module-lexer/lexer.js',
+                     'amaro/dist/index': 'deps/amaro/dist/index.js'
 }
 
 # create option groups
